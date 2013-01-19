a65ee0247a59de75f6c15ef75553eecf82d126bd
Anil Saraswathy <anilgs@gmail.com>
2013-01-19 13:44:52 +0530

    need login validation since login_slug depends on that

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,9 +23,8 @@ class User < ActiveRecord::Base
       c.validates_length_of_password_field_options = { :within => 6..20, :if => :password_required? }
       c.validates_length_of_password_confirmation_field_options = { :within => 6..20, :if => :password_required? }
 
-      #c.validates_length_of_login_field_options = { :minimum => 5, :unless => :omniauthed? }
+      c.validates_length_of_login_field_options = { :minimum => 5, :unless => :omniauthed? }
       #c.validates_format_of_login_field_options = { :with => /\A[\sA-Za-z0-9_-]+\z/, :unless => :omniauthed? }
-      c.validate_login_field = false
       c.validates_length_of_email_field_options = { :within => 3..100, :if => :email_required? }
       c.validates_format_of_email_field_options = { :with => /\A([^@\s]+)@((?:[-a-z0-9A-Z]+\.)+[a-zA-Z]{2,})\z/, :if => :email_required? }
       c.validates_uniqueness_of_email_field_options :case_sensitive => false
